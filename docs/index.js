edb70379fe41dcb802b38cd033431077854a0187
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-15 17:01:39 +0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -30,13 +30,25 @@ form.oninput = function vacCheck() {
   let age = document.getElementById("age").value;
   let vaccinate = document.getElementById("vaccinate").value;
   let dose = document.getElementById("dose").value;
-  let hide = document.getElementById("hide").value;
+  let hide = document.getElementById("hide");
+  let result = document.getElementById("result");
 
   //   console.log("age", age);
-  console.log("vaccinate", vaccinate);
+  //   console.log("vaccinate", vaccinate);
   //   console.log("dose", dose);
   //   console.log("hide", hide);
 
-  vaccinate = "Yes" ? "madarchod" : "bhenchod";
-  console.log("vaccinate", vaccinate);
+  //   if (vaccinate === "Yes") {
+  //     hide.style.display = "block";
+  //   } else {
+  //     hide.style.display = "none";
+  //   }
+
+  age >= 16 && vaccinate === "Yes"
+    ? (hide.style.display = "block")
+    : (hide.style.display = "none");
+
+  age >= 16 && vaccinate === "No"
+    ? (result.innerText = "Please visit your nearest vaccination center ASAP!")
+    : (result.innerText = "You are safe! Thank you!");
 };
