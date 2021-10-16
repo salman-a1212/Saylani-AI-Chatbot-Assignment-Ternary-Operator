8684559171cb0922286d1488dadadb509c8f2128
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-16 05:17:29 +0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,31 +1,3 @@
-// // Javascript code to implement
-// // ternary operator
-
-// // Function to implement
-// // ternary operator without
-// // conditional statements
-// function ternaryOperator(a, b, c) {
-//   // If a is true,
-//   // we return (1 * b) + (!1 * c) i.e. b
-//   // If a is false,
-//   // we return (!1 * b) + (1 * c) i.e. c
-
-//   return !!a * b + !a * c;
-// }
-
-// // Driver code
-// let a = 1,
-//   b = 10,
-//   c = 20;
-// // Function call to output b or c depending on a
-// document.write(ternaryOperator(a, b, c) + "<br>");
-
-// a = 0;
-// // Function call to output b or c depending on a
-// document.write(ternaryOperator(a, b, c) + "<br>");
-
-// // This code is contributed by avanitrachhadiya2155
-
 form.oninput = function vacCheck() {
   let age = document.getElementById("age").value;
   let vaccinate = document.getElementById("vaccinate").value;
@@ -33,30 +5,11 @@ form.oninput = function vacCheck() {
   let hide = document.getElementById("hide");
   let result = document.getElementById("result");
 
-  //   console.log("age", age);
-  //   console.log("vaccinate", vaccinate);
-  //   console.log("dose", dose);
-  //   console.log("hide", hide);
-
-  //   if (vaccinate === "Yes") {
-  //     hide.style.display = "block";
-  //   } else {
-  //     hide.style.display = "none";
-  //   }
-
   age >= 16 && vaccinate === "Yes"
     ? (hide.style.display = "block")
     : (hide.style.display = "none");
 
-  //   dose === "Single"
-  //     ? (result.innerText = "Please complete the vaccination dosage")
-  //     : (result.innerText = "You are safe! Thank you!");
-
   age >= 16 && vaccinate === "No"
     ? (result.innerText = "Please visit your nearest vaccination center ASAP!")
-    : (result.innerText = "You are safe! Thank you!");
-
-  //   age < 16 && vaccinate === "No"
-  //     ? (result.innerText = "You are not eligible for vaccination")
-  //     : (result.innerText = "Stay at home don't go near school :)");
+    : (result.innerText = "You are not eligible for vaccination");
 };
